5bc3a9ee15a779812ff051b2a81d7a63351f4032
NickGodov <nick1985>
2015-09-28 21:02:31 +0300

    Immersive sticky mode

--- app/src/main/java/com/isosystems/smarthotel/MainActivity.java
+++ app/src/main/java/com/isosystems/smarthotel/MainActivity.java
@@ -484,9 +484,16 @@ public class MainActivity extends FragmentActivity {
     private void setFullScreen() {
         try {
             setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-
             // Запрет на отключение экрана
             getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
 
             Process proc = null;
             String ProcID = "79"; //HONEYCOMB AND OLDER
@@ -529,6 +536,8 @@ public class MainActivity extends FragmentActivity {
     @Override
     protected void onResume() {
         super.onResume();
+
+        setFullScreen();
 
         // Считывание настроек хранителя экрана
         readScreensaverPreferences();
